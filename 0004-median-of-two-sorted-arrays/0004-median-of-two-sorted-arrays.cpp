7d8e1d837197814bac82554730cdb6c31e34dc8d
Trigun <triguntamragouri@gmail.com>
2025-06-30 22:24:46 +0530

    Time: 0 ms (100%), Space: 95.1 MB (59.9%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-  double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
-    int n1 = nums1.size(), n2 = nums2.size();
-    int i = 0, j = 0;
-    vector<int> nums3;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        int n = n1 + n2;
+        int ind1 = (n - 1) / 2;
+        int ind2 = n / 2;
 
-    while(i < n1 && j < n2) {
-      if(nums1[i] < nums2[j])
-        nums3.push_back(nums1[i++]);
-      else
-        nums3.push_back(nums2[j++]);
+        int i = 0, j = 0, cnt = 0;
+        int curr = 0, prev = 0;
+
+        while (i < n1 || j < n2) {
+            prev = curr;
+
+            if (i < n1 && (j >= n2 || nums1[i] < nums2[j])) {
+                curr = nums1[i++];
+            } else {
+                curr = nums2[j++];
+            }
+
+            if (cnt == ind2) {
+                if (n % 2 == 1) return curr;
+                return (prev + curr) / 2.0;
+            }
+
+            cnt++;
+        }
+
+        return 0.0; // fallback, should never reach here
     }
-
-    while(i < n1)
-      nums3.push_back(nums1[i++]);
-    while(j < n2)
-      nums3.push_back(nums2[j++]);
-
-    int n = n1 + n2;
-    if(n % 2 == 1)
-      return (double)nums3[n/2];
-    else
-      return ((double)nums3[n / 2] + (double)nums3[(n / 2) - 1]) / 2.0;
-  }
 };
