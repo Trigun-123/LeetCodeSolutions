48e3392889b49c4f93270d2d4e504cf30413302a
Trigun <triguntamragouri@gmail.com>
2025-07-07 11:07:56 +0530

    Time: 0 ms (100%), Space: 13.9 MB (78.77%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,18 +1,24 @@
 class Solution{	
-	public:		
-		vector<int> singleNumber(vector<int>& nums){
-            int n = nums.size();
-			vector<int> result;
-            unordered_map<int, int> mpp;
+public:		
+	vector<int> singleNumber(vector<int>& nums){
+        int n = nums.size();
+		long XOR = 0;
 
-            for(int i = 0; i < n; i++)
-                mpp[nums[i]]++;
+        for(int i = 0; i < n; i++)
+            XOR ^= nums[i];
 
-            for(auto it : mpp) {
-                if(it.second == 1)
-                    result.push_back(it.first);
-            }
-            sort(result.begin(), result.end());
-            return result;
-		}
+        int rightmost = (XOR & (XOR - 1)) ^ XOR;
+        int bucket1 = 0, bucket2 = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] & rightmost)
+                bucket1 ^= nums[i];
+            else
+                bucket2 ^= nums[i];
+        } 
+        if(bucket1 < bucket2)
+            return {bucket1, bucket2};
+        else
+            return {bucket2, bucket1};
+	}
 };
