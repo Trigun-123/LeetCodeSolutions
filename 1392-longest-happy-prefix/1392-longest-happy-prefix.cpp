da93701fcf4874b96ce707f2f91cddc1c07ae12d
Trigun <triguntamragouri@gmail.com>
2025-07-09 12:27:42 +0530

    Time: 0 ms (100%), Space: 24.9 MB (8.32%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,32 @@
+class Solution{
+private:
+    vector<int> computeLPS(string s) {
+        int n = s.size(); 
+        vector<int> LPS(n, 0);
+        int i = 1, j = 0;
+    
+        while(i < n) {
+            if(s[i] == s[j]) {
+                LPS[i] = j+1;
+                i++, j++;
+            }
+            else {
+                while(j > 0 && s[i] != s[j])
+                    j = LPS[j-1];
+                
+                if(s[i] == s[j]) {
+                    LPS[i] = j+1;
+                    j++;
+                }
+                i++;
+            }
+        }
+        return LPS; 
+    }
+
+public:		
+	string longestPrefix(string s) {
+        vector<int> LPS = computeLPS(s);
+	    return s.substr(0, LPS[s.size() - 1]);
+	}
+};
