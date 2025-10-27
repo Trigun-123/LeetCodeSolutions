d5583daca2c60fe17245be399fdd3abb245c666b
Trigun <triguntamragouri@gmail.com>
2025-10-27 14:42:45 +0530

    Time: 6 ms (38.43%), Space: 18.5 MB (50.07%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> adj[numCourses];
-        
-        // Build adjacency list: v → u means u depends on v
+    
         for (auto it : prerequisites) {
             int u = it[0];
             int v = it[1];
             adj[v].push_back(u);
         }
-        
-        // Step 1: Calculate indegree of each node
+       
         vector<int> inDegree(numCourses, 0);
         for (int i = 0; i < numCourses; i++) {
             for (auto it : adj[i]) {
@@ -18,14 +16,12 @@ public:
             }
         }
 
-        // Step 2: Push all nodes with indegree 0 into queue
         queue<int> q;
         for (int i = 0; i < numCourses; i++) {
             if (inDegree[i] == 0)
                 q.push(i);
         }
 
-        // Step 3: Perform Kahn’s Algorithm (BFS for topological sort)
         vector<int> topo;
         while (!q.empty()) {
             int node = q.front();
@@ -39,7 +35,6 @@ public:
             }
         }
 
-        // Step 4: If topological sort includes all nodes, return it
         if (topo.size() < numCourses)
             return {};
         
