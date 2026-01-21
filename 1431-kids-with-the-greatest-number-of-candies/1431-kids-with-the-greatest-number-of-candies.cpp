583cba83e048633cc61aa599513c70790142f773
Trigun <triguntamragouri@gmail.com>
2026-01-21 22:23:38 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.22%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n = candies.size();
+        int maxCandies = *max_element(candies.begin(), candies.end());
+        
+        vector<bool> result(n);
+        
+        for(int i = 0; i < n; i++) {
+            if(candies[i] + extraCandies >= maxCandies) {
+                result[i] = true;
+            } 
+            else {
+                result[i] = false;
+            }
+        }
+        
+        return result;
+    }
+};
