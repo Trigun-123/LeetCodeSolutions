c8aedb7645d8a2787a95ca9e7b8945d5e36006df
Trigun <triguntamragouri@gmail.com>
2026-01-26 09:03:23 +0530

    Time: 0 ms (100%), Space: 15 MB (97.3%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size(), n = mat[0].size();
+
+        if((m * n) != (r * c)) {
+            return mat;
+        }
+
+        vector<vector<int>> result(r, vector<int>(c, 0));
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                int k = i * n + j;
+                result[k / c][k % c] = mat[i][j];
+            }
+        }
+
+        return result;
+    }
+};
