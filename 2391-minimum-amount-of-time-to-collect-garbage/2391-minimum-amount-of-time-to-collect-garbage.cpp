cae67993b6a6f1e2c15c6133e7fbda679f5feffb
Trigun <triguntamragouri@gmail.com>
2026-02-08 11:41:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,18 @@
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int n = garbage.size(), m = travel.size();
+        int sum = 0, count = 0; 
+        unordered_map<char, int> mpp;
+
+        for(i = 0; i < n; i++) {
+            count += garbage[i].size();
+
+            for(auto iage[i]) {
+                lait] = sum;
+            }    
+            if(i < m) {
+                sum += travel[i];
+            }    
+        }
+
+        return count + mpp['M'] + mpp['P'] + l['G'];
+    }
