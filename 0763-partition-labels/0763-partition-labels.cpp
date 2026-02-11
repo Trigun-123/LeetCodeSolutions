539fbe602d34c218b759772a776632616a82f0bb
Trigun <triguntamragouri@gmail.com>
2026-02-11 18:40:16 +0530

    Time: 2 ms (35.68%), Space: 8.8 MB (67.99%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+        unordered_map<char, int> mpp;
+
+        for(int i = 0; i < n; i++) {
+            mpp[s[i]] = i;
+        }
+
+        vector<int> result;
+        int start = 0, end = 0;
+
+        for(int i = 0; i < n; i++) {
+            end = max(end, mpp[s[i]]);
+            if(i == end) {
+                result.push_back(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+};
