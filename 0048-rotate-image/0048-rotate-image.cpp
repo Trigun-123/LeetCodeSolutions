34c9e2c6c61ddcf1262a57b1a1300bc983e13dd3
TrigunDev <triguntamragouri@gmail.com>
2026-05-04 08:26:39 +0530

    Time: 0 ms (100%), Space: 10 MB (92.7%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,14 +3,14 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        // Transpose the matrix
-        for(int i = 0; i <= n-2; i++) {
-            for(int j = i+1; j <= n-1; j++)
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
                 swap(matrix[i][j], matrix[j][i]);
+            }
         }
 
-        // Reverse each row of the matrix
-        for(int i = 0; i <= n-1; i++)
+        for(int i = 0; i < n; i++) {
             reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
